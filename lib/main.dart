47df2976978d16cda035b8b0892e27e85fa78eb5
punitkumar141205 <punitkumar141205@gmail.com>
2024-11-25 21:00:07 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: const Text(
+            'Flitter Layout',
+            style: TextStyle(fontSize: 34),
+          ),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Item 1',
+                style: TextStyle(fontSize: 45),
+              ),
+              Text(
+                'Item 2',
+                style: TextStyle(fontSize: 45),
+              ),
+            ],
+          ),
+        ),
+        drawer: Drawer(),
+      ),
+    );
+  }
+}
